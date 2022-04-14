4347a760a10a84df450759c5a98a6f2858d71b2d
Adam Reese <adam@reese.io>
2022-04-13 19:09:55 -0700

    Add script for starting all services

---
+++ serve.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Kill running jobs on exit
+trap 'kill $(jobs -p)' EXIT
+
+# Start HTTP handlers
+spin up --log-dir ./log --file spin.toml &
+
+# Start Redis handler
+spin up --log-dir ./log --file morsel_event/spin.toml &
+
+wait
