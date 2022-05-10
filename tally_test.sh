2958ca01fdd028149f777fcac1bbd2c93c2d230b
Vaughn Dice <vaughn.dice@fermyon.com>
2022-05-10 08:27:56 -0600

    feat(tally_test.sh): add -L to curl to follow redirects
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- tally_test.sh
+++ tally_test.sh
@@ -5,15 +5,18 @@ set -euo pipefail
 
 ENDPOINT="${ENDPOINT:-localhost:3000}"
 
+# Follow redirects
+CURL="curl -L"
+
 # Create a new session and get the ULID
-ulid=$(curl -s "${ENDPOINT}/session" | jq -r '.id')
+ulid=$(${CURL} -s "${ENDPOINT}/session" | jq -r '.id')
 
 echo "Session ULID: ${ulid}"
 
 # Fake some correct answers
 for demand in fish fish chicken beef veg veg; do
-  curl -s "${ENDPOINT}/tally?ulid=${ulid}&food=${demand}&correct=true" >/dev/null
+  ${CURL} -s "${ENDPOINT}/tally?ulid=${ulid}&food=${demand}&correct=true" >/dev/null
 done
 
 # Get the score
-curl "${ENDPOINT}/score?ulid=${ulid}"
+${CURL} "${ENDPOINT}/score?ulid=${ulid}"
